a16d2b2189b9f335011c9e8cd8368792ff228da5
izadimran-mon <imuh0001@student.monash.edu>
2021-10-09 03:00:50 +1100

    Add project files.

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using FIT5032_Assignment_2.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(FIT5032_Assignment_2.Areas.Identity.IdentityHostingStartup))]
+namespace FIT5032_Assignment_2.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ Controllers/RoleController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace FIT5032_Assignment_2.Controllers
+{
+    [Authorize(Roles="Organisation")]
+    public class RoleController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public RoleController(RoleManager<IdentityRole> roleManager)
+        {
+            this._roleManager = roleManager;
+        }
+        public IActionResult Index()
+        {
+            var roles = _roleManager.Roles.ToList();
+            return View(roles);
+        }
+        public IActionResult Create()
+        {
+            return View(new IdentityRole());
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(IdentityRole role)
+        {
+            await _roleManager.CreateAsync(role);
+            return RedirectToAction("Index");
+        }
+    }
+}
